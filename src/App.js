33831be5792dd0f14c249e838b82f0cd98860867
Dave <dave.cadelina@outlook.com>
2021-10-15 01:18:26 +1100

    aligned map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import map from './assets/map.png'
 
 //-- API's
 //-- https://www.beliefmedia.com.au/australian-postal-codes
@@ -8,6 +8,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="app__content">
+          <div className="panel"></div>
+          <div className="map__container">
+            <img src={map} alt="map" className="map" />
+            <div className="WA">WA</div>
+          </div>
+          
+      </div>
       
     </div>
   );
