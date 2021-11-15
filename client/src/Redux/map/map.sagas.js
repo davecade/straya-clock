e2e7fb21c6408296cbc7f1fd7f3752600a4e046d
Dave <dave.cadelina@outlook.com>
2021-11-15 22:29:41 +1100

    added nodejs backend

---
+++ client/src/Redux/map/map.sagas.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import { takeLatest, put, all, call, } from "redux-saga/effects";
+import { MapActionTypes } from "./map.types";
+import {
+    updateCurrentTime,
+    updatePostcodeData,
+    updateSelected,
+    setLoading
+} from "./map.actions";
+
+let firstLoad = true
+
+export function* fetchMapDataAsync() {
+    try {
+
+        const fetchCurrentTime = yield axios.get('/map')
+        const currentTime = fetchCurrentTime.data
+        yield put(updateCurrentTime(currentTime))
+
+        if(firstLoad) {
+            yield put(setLoading(false))
+            firstLoad = false
+        }
+
+    } catch(error) {
+        console.log("ERROR", error)
+        if(firstLoad) {
+            yield put(setLoading(false))
+            firstLoad = false
+        }
+    }
+}
+
+export function* fetchPostcodeDataAsync({payload}) {
+    try {
+        yield put(setLoading(true))
+        const postcodeData = yield axios.get(`/postcode/${payload}`)
+        yield put(updatePostcodeData(postcodeData))
+        yield put(updateSelected(postcodeData[0].state.abbreviation))
+        yield put(setLoading(false))
+
+    } catch(error) {
+        yield put(updateSelected(""))
+        yield put(setLoading(false))
+        console.log("ERROR", error)
+
+    }
+}
+
+export function* onFetchMapDataStart() {
+    yield takeLatest(MapActionTypes.FETCH_MAP_DATA_START, fetchMapDataAsync)
+}
+
+export function* onFetchPostcodeData() {
+    yield takeLatest(MapActionTypes.FETCH_POSTCODE_DATA, fetchPostcodeDataAsync)
+}
+
+export function* mapSagas() {
+    yield all([
+        call(onFetchMapDataStart),
+        call(onFetchPostcodeData)
+    ])
+}
---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+
+if(process.env.NODE_ENV !== 'production') require('dotenv').config()
+
+const app = express()
+const PORT = 4000
+
+app.use(express.json())
+app.use(cors())
+app.use(express.urlencoded({extended:true}));
+
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+    })
+}
+
+
+app.get('/map', async (req, res) => {
+    const requestOne = axios.get(`https://worldtimeapi.org/api/timezone/Australia/Sydney`)
+    const requestTwo = axios.get(`https://worldtimeapi.org/api/timezone/Australia/Brisbane`)
+    const requestThree = axios.get(`https://worldtimeapi.org/api/timezone/Australia/Melbourne`)
+    const requestFour = axios.get(`https://worldtimeapi.org/api/timezone/Australia/Adelaide`)
+    const requestFive = axios.get(`https://worldtimeapi.org/api/timezone/Australia/Darwin`)
+    const requestSix = axios.get(`https://worldtimeapi.org/api/timezone/Australia/Perth`)
+    const requestSeven = axios.get(`https://worldtimeapi.org/api/timezone/Australia/Hobart`)
+    
+    const mapData = await axios.all([
+        requestOne,
+        requestTwo,
+        requestThree,
+        requestFour,
+        requestFive,
+        requestSix,
+        requestSeven
+    ])
+
+    const currentTime = {
+        NSW: mapData[0].data.datetime.slice(11, 19),
+        QLD: mapData[1].data.datetime.slice(11, 19),
+        VIC: mapData[2].data.datetime.slice(11, 19),
+        SA: mapData[3].data.datetime.slice(11, 19),
+        NT: mapData[4].data.datetime.slice(11, 19),
+        WA: mapData[5].data.datetime.slice(11, 19),
+        TAS: mapData[6].data.datetime.slice(11, 19),
+    }
+    res.send(currentTime)
+})
+
+app.get('/postcode/:code', async (req, res) => {
+    const fetchPostcodeData = await axios.get(`https://api.jsacreative.com.au/v1/suburbs?postcode=${req.params.code}`)
+    const postcodeData = fetchPostcodeData.data
+    res.send(postcodeData)
+
+})
+
+
+//-- Runs server
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`)
+})
