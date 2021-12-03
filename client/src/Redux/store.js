decbdd7d06c0a1b340e2d17b7143eb1f06612f5b
Dave <dave.cadelina@outlook.com>
2021-12-03 16:21:44 +1100

    added logger middleware

--- client/src/Redux/store.js
+++ client/src/Redux/store.js
@@ -13,7 +13,7 @@ const middlewares = [sagaMiddleware];
 // -- Checks what environment the app is running on
 // -- Only loads logger if in development environment
 if(process.env.NODE_ENV === 'development') {
-    middlewares.push()
+    middlewares.push(logger)
 }
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares))
